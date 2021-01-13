22d5159cdc8ef2855336ab1a006849d810c99fd1
Brett Higgins <brett.higgins@gmail.com>
2021-01-13 13:43:09 -0500

    Create initial test case

---
+++ snapshots/snap_test_snapshot_update.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['UpdateTest::test_snapshot 1'] = {
+    'foo': 42
+}
---
+++ test_snapshot_update.py
@@ -0,0 +1,7 @@
+import snapshottest
+
+class UpdateTest(snapshottest.TestCase):
+    def test_snapshot(self):
+        self.assertMatchSnapshot({
+            "foo": 42
+        })
