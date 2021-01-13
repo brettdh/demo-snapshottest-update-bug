ca85c3149cedd29ea581f0c946ec028331c267b6
Brett Higgins <brett.higgins@gmail.com>
2021-01-13 13:43:52 -0500

    Update test without updating snapshot

--- test_snapshot_update.py
+++ test_snapshot_update.py
@@ -3,5 +3,6 @@ import snapshottest
 class UpdateTest(snapshottest.TestCase):
     def test_snapshot(self):
         self.assertMatchSnapshot({
-            "foo": 42
+            "foo": 42,
+            "bar": 123,
         })
